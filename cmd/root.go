9d45724439cb6e775d6aea270fea8b5fb2efc0c6
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-05-20 18:11:23 +0200

    [feat] Add first setup

---
+++ cmd/root.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "EasyCD",
+	Short: "A command line tool for creating waypoints for your file system, allowing you to easily go to common directories using a short command.",
+	Long: `A command line tool for creating waypoints for your file system, allowing you to easily go to common directories using a short command.
+
+To create a waypoint pointing to the current directory, simply run the command:
+	ecd create <waypoint name>
+
+After creating a waypoint, you can go to it using the command:
+	ecd cd <waypoint name>
+
+You can also list all waypoints using the command:
+	ecd list
+`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.EasyCD.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/rafaeltab/EasyCD/cmd"
+
+func main() {
+	cmd.Execute()
+}
