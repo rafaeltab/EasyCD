9de38a730e57f459edb77aafd0251622d571daa6
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-05-20 19:12:12 +0200

    [feat] Actual file store in users home directory

--- helper/waypointStore.go
+++ helper/waypointStore.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 type Waypoint struct {
@@ -11,15 +12,13 @@ type Waypoint struct {
 	Path string
 }
 
-var store = `[
-	{
-		"Name": "home",
-		"Path": "/home/user"
-	}
-]`
-
 func AddWaypoint(name string, directory string) {
 	fmt.Println("Creating waypoint", name, "in", directory)
+
+	if(GetWaypoint(name).Name != "") {
+		fmt.Println("Waypoint with name", name, "already exists")
+		os.Exit(1)
+	}
 
 	var currentStore []Waypoint = GetWaypoints()
 
@@ -29,8 +28,6 @@ func AddWaypoint(name string, directory string) {
 	})	
 
 	saveStore(currentStore)
-
-	fmt.Println(currentStore)
 }
 
 func GetWaypoints() []Waypoint {
@@ -44,31 +41,43 @@ func GetWaypoint(name string) Waypoint {
 			return currentStore[i]
 		}
 	}
-
-	fmt.Println("Waypoint", name, "not found")
-	os.Exit(1)
 	return Waypoint{}
 }
+
+var homeDir, _ = os.UserHomeDir()
+var storeDir = filepath.Join(homeDir, ".easycd.json")
 
 func getStore() []Waypoint {
 	var currentStore []Waypoint
 
-	var err = json.Unmarshal([]byte(store), &currentStore)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if _, err := os.Stat(storeDir); os.IsNotExist(err) {
+		os.Create(storeDir)
+		os.WriteFile(storeDir, []byte("[]"), 0644)
 	}
+
+	var store, err = os.ReadFile(storeDir)
+	checkError(err)
+
+	err = json.Unmarshal([]byte(store), &currentStore)
+	checkError(err)
 
 	return currentStore
 }
 
 func saveStore(newStore []Waypoint) {
-	var marshalledStore, err = json.Marshal(store)
+	var marshalledStore, err = json.Marshal(newStore)
 
-	if(err != nil) {
-		fmt.Println(err)
-		os.Exit(1)
+	checkError(err)
+
+	if _, err := os.Stat(storeDir); os.IsNotExist(err) {
+		os.Create(storeDir)
 	}
 
-	store = string(marshalledStore)
+	os.WriteFile(storeDir, []byte(string(marshalledStore)), 0644)
+}
+
+func checkError(err error){
+	if err != nil {
+		panic(err)
+	}
 }
